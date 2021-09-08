e47020109a621f66f354c652765e03b7a683643d
Penélope Araújo <pmpa@cin.ufpe.br>
2021-09-08 14:28:50 -0300

    chore: project setup

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Penélope Araújo on 08/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ palettes/palettes.h
@@ -0,0 +1,18 @@
+//
+//  palettes.h
+//  palettes
+//
+//  Created by Penélope Araújo on 08/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for palettes.
+FOUNDATION_EXPORT double palettesVersionNumber;
+
+//! Project version string for palettes.
+FOUNDATION_EXPORT const unsigned char palettesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <palettes/PublicHeader.h>
+
+
